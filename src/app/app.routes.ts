2413f5836f0872722a7ec9af05aa22eeb5a3f0ea
Naveenganesh <88182380+naveenganesh124@users.noreply.github.com>
2025-12-26 17:08:05 +0530

    login page implementation
    * login page implementation
    
    * product page rediesign with bootstrap

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,23 +1,32 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './core/guards/auth.guard';
 
 export const routes: Routes = [
+
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./features/auth/pages/login/login.component')
+        .then(m => m.LoginComponent)
+  },
+
+
   {
     path: '',
     loadComponent: () =>
       import('./layouts/main-layout/main-layout.component')
         .then(m => m.MainLayoutComponent),
+    canActivate: [authGuard],
     children: [
       {
         path: 'products',
         loadComponent: () =>
           import('./features/products/pages/product-list/product-list.component')
             .then(m => m.ProductListComponent)
-      },
-      {
-        path: '',
-        redirectTo: 'products',
-        pathMatch: 'full'
       }
     ]
-  }
+  },
+
+  // fallback
+  { path: '**', redirectTo: 'login' }
 ];
---
+++ src/app/core/services/auth.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private readonly TOKEN_KEY = 'auth_token';
+
+  setToken(token: string): void {
+    localStorage.setItem(this.TOKEN_KEY, token);
+  }
+
+  getToken(): string | null {
+    return localStorage.getItem(this.TOKEN_KEY);
+  }
+
+  isLoggedIn(): boolean {
+    return !!this.getToken();
+  }
+
+  logout(): void {
+    localStorage.removeItem(this.TOKEN_KEY);
+  }
+}
